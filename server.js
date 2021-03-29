8236910eb7b8c68bb1230940e4345fd6526b1684
stevenspicher <71337603+stevenspicher@users.noreply.github.com>
2021-03-28 22:09:48 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express();
+const PORT = 5000;
+
+
+const info = [
+    { name: "JohnSnow", password: "knowsnothing" }
+];
+
+//add route to send back info:
+//
+//
+// 
+// ***complete this section****
+
+app.listen(PORT, () => console.log(`listening, ${PORT}`));
